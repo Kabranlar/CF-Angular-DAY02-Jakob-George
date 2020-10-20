6426e9c2ed307a3224185afcca9bd536f4672c4a
George Ciocan <george.ciocan21@gmail.com>
2020-10-20 14:04:31 +0200

    fixes form

--- carRental/src/app/contact/contact.component.ts
+++ carRental/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  contactInfo = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    address: new FormControl('', Validators.required),
+    gridCheck: new FormControl('', Validators.required)
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSubmit() {
+    let a = this.contactInfo.value
+    console.log(a)
+  }
 }
